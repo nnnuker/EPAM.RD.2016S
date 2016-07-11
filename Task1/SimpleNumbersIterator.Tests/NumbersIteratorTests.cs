b3d63e033c3cf6f33ce37a53e0cc6c335921e5f9
Valery_Larkovich <Valery_Larkovich@EPBYMINW2230>
2016-07-11 19:46:42 +0300

    Initial commit

---
+++ Task1/SimpleNumbersIterator.Tests/NumbersIteratorTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace SimpleNumbersIterator.Tests
+{
+    [TestClass]
+    public class NumbersIteratorTests
+    {
+        [TestMethod]
+        public void GetEnumerator_TakeTen_ReturnTen()
+        {
+            var result = new List<int>();
+            var expected = new List<int> { 1, 2, 3, 5, 7, 11, 13, 17, 19, 23};
+
+            var enumerator = NumbersIterator.GetEnumerator();
+            for (int i = 0; i < 10; i++)
+            {
+                if (enumerator.MoveNext())
+                {
+                    result.Add(enumerator.Current);
+                }
+            }
+
+            CollectionAssert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ Task1/SimpleNumbersIterator/NumbersIterator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleNumbersIterator
+{
+    public static class NumbersIterator
+    {
+        public static IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 1; i < int.MaxValue; i++)
+            {
+                if (IsSimple(i))
+                {
+                    yield return i;
+                }
+            }
+        }
+
+        private static bool IsSimple(int num)
+        {
+            for (int i = 2; i < num; i++)
+            {
+                if (num % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
