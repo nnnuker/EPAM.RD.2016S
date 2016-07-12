3a918dc6c8cf28848bb028fd67e5e86e4cf32a65
Valery_Larkovich <Valery_Larkovich@EPBYMINW2230>
2016-07-12 17:35:20 +0300

    Steps minified

--- Task1/SimpleNumbersIterator/NumbersIterator.cs
+++ Task1/SimpleNumbersIterator/NumbersIterator.cs
@@ -21,7 +21,7 @@ namespace SimpleNumbersIterator
 
         private static bool IsSimple(int num)
         {
-            for (int i = 2; i < num; i++)
+            for (int i = 2; i <= num / 2; i++)
             {
                 if (num % i == 0)
                 {
