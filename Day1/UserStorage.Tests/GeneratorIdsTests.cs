cfac3a9660e569f68bbefcbace30af93dc80ce89
Valery_Larkovich <Valery_Larkovich@EPBYMINW2230>
2016-07-13 14:35:11 +0300

    Added tests.

---
+++ Day1/UserStorage.Tests/GeneratorIdsTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UserStorage.Infrastructure;
+
+namespace UserStorage.Tests
+{
+    [TestClass]
+    public class GeneratorIdsTests
+    {
+        [TestMethod]
+        public void GetId_TryGetFirstId_ReturnOne()
+        {
+            IGenerator generator = new GeneratorIds();
+            var result = generator.GetId();
+
+            Assert.AreEqual(1, result);
+        }
+    }
+}
---
+++ Day1/UserStorage.Tests/StorageTests.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UserStorage.Services;
+using UserStorage.Entities;
+using UserStorage.Infrastructure;
+using UserStorage.Storages;
+
+namespace UserStorage.Tests
+{
+    [TestClass]
+    public class StorageTests
+    {
+        private IStorage<User> storage;
+        private User user = new User
+        {
+            DateOfBirth = new DateTime(1994, 9, 25),
+            FirstName = "My",
+            LastName = "Name",
+            Gender = GenderEnum.Male,
+            PersonalId = "AB231223",
+            Visa = new VisaRecord { Country = "Belarus" }
+        };
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            storage = new Storage(new MemoryUserRepository(), new ValidatorUsers(), new GeneratorIds());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Add_NotValidUser_ThrowsArgumentException()
+        {
+            var user = new User();
+
+            storage.Add(user);
+        }
+
+        [TestMethod]
+        public void Add_ValidUser_ReturnsUserId()
+        {
+
+
+            var result = storage.Add(user);
+
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void Add_TwoUsersWithSameNames_ReturnsUserId()
+        {
+            storage.Add(user);
+            var result = storage.Add(user);
+
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void Add_TwoUsers_ReturnsUserId()
+        {
+            storage.Add(user);
+
+            User user1 = new User
+            {
+                FirstName = "His",
+                LastName = "Name",
+                PersonalId = "AB231223",
+                Visa = new VisaRecord { Country = "Belarus" }
+            };
+
+            var result = storage.Add(user1);
+
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void Delete_ByCorrectId_ExpectDelete()
+        {
+            storage.Add(user);
+            storage.Delete(user.Id);
+
+            Assert.AreEqual(0, storage.Search(u => u.Id == 1));
+        }
+
+        [TestMethod]
+        public void Delete_ByUser_ExpectDelete()
+        {
+            storage.Add(user);
+            storage.Delete(user);
+
+            Assert.AreEqual(0, storage.Search(u => u.Id == 1));
+        }
+
+        [TestMethod]
+        public void Search_ByName_ReturnOne()
+        {
+            storage.Add(user);
+
+            var result = storage.Search(u => u.FirstName == "My");
+
+            Assert.AreEqual(1, result);
+        }
+    }
+}
