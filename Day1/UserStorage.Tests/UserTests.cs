e18e160cfe3227028dba2242f29101adb51e095f
Valery_Larkovich <Valery_Larkovich@EPBYMINW2230>
2016-07-13 12:55:33 +0300

    Added one test

--- Day1/UserStorage.Tests/UserTests.cs
+++ Day1/UserStorage.Tests/UserTests.cs
@@ -7,30 +7,40 @@ namespace UserStorage.Tests
     [TestClass]
     public class UserTests
     {
+        private User user = new User
+        {
+            Id = 10,
+            DateOfBirth = new DateTime(1994, 9, 25),
+            FirstName = "My",
+            LastName = "Name",
+            Gender = GenderEnum.Male,
+            PersonalId = "AB231223"
+        };
+
+        private User user1 = new User
+        {
+            Id = 10,
+            DateOfBirth = new DateTime(1994, 9, 25),
+            FirstName = "My",
+            LastName = "Name",
+            Gender = GenderEnum.Male,
+            PersonalId = "AB231223"
+        };
+
         [TestMethod]
         public void GetHashCode_TwoUsersHashCompare_ReturnIsValid()
         {
-            User user = new User
-            {
-                Id = 10,
-                DateOfBirth = new DateTime(1994, 9, 25),
-                FirstName = "My",
-                LastName = "Name",
-                Gender = GenderEnum.Male,
-                PersonalId = "AB231223"
-            };
-
-            User user1 = new User
-            {
-                Id = 10,
-                DateOfBirth = new DateTime(1994, 9, 25),
-                FirstName = "My",
-                LastName = "Name",
-                Gender = GenderEnum.Male,
-                PersonalId = "AB231223"
-            };
+            user1.Gender = GenderEnum.Male;
 
             Assert.IsTrue(user.GetHashCode() == user1.GetHashCode());
+        }
+
+        [TestMethod]
+        public void GetHashCode_TwoDiffUsersHashCompare_ReturnIsNotValid()
+        {
+            user1.Gender = GenderEnum.Female;
+
+            Assert.IsFalse(user.GetHashCode() == user1.GetHashCode());
         }
     }
 }
