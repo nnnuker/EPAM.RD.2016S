8be2dca03069920f034919ac77c9498528733dfd
Valery_Larkovich <Valery_Larkovich@EPBYMINW2230>
2016-07-21 19:55:48 +0300

    Day 7

---
+++ Day7/ConstTask/TaskConsole/MyClassLibrary/ImportantEnum.cs
@@ -0,0 +1,12 @@
+﻿namespace MyClassLibrary
+{
+  public enum ImportantEnum
+  {
+        Black = 3,
+    White = 4,
+    Red = 0,    //0
+    Green = 1,  //1
+    Blue = 5,
+    Orange = 2 //2
+  }
+}
---
+++ Day7/UnitTestProjectCollection/UnitTestProjectCollection/BlockCollClass.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace UnitTestProjectCollection
+{
+  public class BlockCollClass
+  {
+    protected ConcurrentBag<int> bc;
+
+    private void producer()
+    {
+      for (int i = 0; i < 100; i++)
+      {
+        bc.Add(i * i);
+        Debug.WriteLine("Create " + i * i);
+      }
+    }
+
+    private void consumer()
+    {
+        int res = 0;
+      while (bc.TryTake(out res))
+      {
+        Debug.WriteLine("Take: " + res);
+      }
+    }
+
+    public void Start()
+    {
+      bc = new ConcurrentBag<int>();
+
+      Task Pr = new Task(producer);
+      Task Cn = new Task(consumer);
+
+      Pr.Start();
+      Cn.Start();
+
+      try
+      {
+        Task.WaitAll(Cn, Pr);
+      }
+      finally
+      {
+        Cn.Dispose();
+        Pr.Dispose();
+      }
+    }
+  }
+}
